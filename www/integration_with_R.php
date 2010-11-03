8ecf8408c926321f43a69b98bbabf76513bbb933
mariotomo <mariotomo@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2010-11-03 08:55:31 +0000

    adding a short howto (withCallingHandlers) as suggested by Henrico Witvliet. (entry page)

---
+++ www/integration_with_R.php
@@ -0,0 +1,18 @@
+<html>
+<head>
+<title>tutorial : hierarchical loggers</title>
+<link href="additions.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+<blockquote>
+<p>
+<a href="sample_session.php">[back to the tutorial]</a> &nbsp;
+</p>
+</blockquote>
+
+
+<?php include("integration_with_R.shtml"); ?> 
+
+</body>
+</html>
